5aab5a9c98065eafb69e8e3ee02f2127e58e1a41
Максим Рожков <solvent71@yandex.ru>
2019-09-19 12:06:21 +0300

    Добавлен пиксель

--- local/templates/dial/includes/_head.php
+++ local/templates/dial/includes/_head.php
@@ -6,6 +6,7 @@
 <?
 $APPLICATION->SetAdditionalCSS('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900&display=swap&subset=cyrillic');
 $APPLICATION->SetAdditionalCSS(TEMPLATE_PATH . '/css/normalize.css');
+$APPLICATION->SetAdditionalCSS(TEMPLATE_PATH . '/libs/swiper/swiper.min.css');
 $APPLICATION->SetAdditionalCSS(TEMPLATE_PATH . '/css/common.css');
 $APPLICATION->SetAdditionalCSS(TEMPLATE_PATH . '/css/style.css');
 $APPLICATION->SetAdditionalCSS(TEMPLATE_PATH . '/libs/fancybox/jquery.fancybox.min.css');
@@ -16,6 +17,7 @@ $APPLICATION->AddHeadScript(TEMPLATE_PATH . '/libs/jquery-3.3.1.min.js');
 $APPLICATION->AddHeadScript(TEMPLATE_PATH . '/libs/inputmask.js');
 $APPLICATION->AddHeadScript(TEMPLATE_PATH . '/libs/svg4everybody.min.js');
 $APPLICATION->AddHeadScript(TEMPLATE_PATH . '/libs/fancybox/jquery.fancybox.min.js');
+$APPLICATION->AddHeadScript(TEMPLATE_PATH . '/libs/swiper/swiper.min.js');
 $APPLICATION->AddHeadScript(TEMPLATE_PATH . '/js/script.js');
 $APPLICATION->ShowHead();
 /*use \Bitrix\Main\Service\GeoIp;
@@ -38,4 +40,22 @@ $APPLICATION->SetPageProperty('ogImage', 'https://dialweb.ru/local/templates/dia
 <meta property="og:image" content="<?$APPLICATION->ShowProperty("ogImage")?>">
 
 <script>(function(w, c){(w[c]=w[c]||[]).push(function(){new zTracker({"id":"7df90b567603575ec8a6c40fa3cd7180695","metrics":{"metrika":"199858"}});});})(window, "zTrackerCallbacks");</script>
-<script async id="zd_ct_phone_script" src="https://my.zadarma.com/js/ct_phone.min.js"></script>
+<script async id="zd_ct_phone_script" src="https://my.zadarma.com/js/ct_phone.min.js"></script<!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '754599888292602');
+    fbq('track', 'PageView');
+</script>
+<noscript>
+    <img height="1" width="1"
+         src="https://www.facebook.com/tr?id=754599888292602&ev=PageView
+&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
